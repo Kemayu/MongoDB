888c45047c6aca40282f050cc260c09f5dff8424
MiracOK <mirac.demirci2@etu.univ-lorraine.fr>
2025-12-06 18:07:01 +0100

    Feat (ex 1, 2 ,3) : Mise en place de MongoDB et des exo 1 2 3 Fix (Version) : version du mongodb change

--- app/public/index.php
+++ app/public/index.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: canals5
@@ -13,3 +14,41 @@ require_once __DIR__ . "/../src/vendor/autoload.php";
 $c = new Client("mongodb://mongo");
 echo "connected to mongo <br>";
 
+$db = $c->__get("chopizza");
+$produit = $db->__get("Produit");
+$cursor = $produit->find([], [
+    'typeMap' => [
+        'root' => 'array',
+        'rument' => 'array',
+        'array' => 'array'
+    ]
+]);
+//1
+echo "1) Liste des Produits : <br>";
+foreach ($cursor as $p) {
+    if (isset($p['numero']) && isset($p['categorie']) && isset($p['libelle'])) {
+        echo "N° " . $p['numero'] . " | Catégorie : " . $p['categorie'] . " | Libelle : " . $p['libelle'] . " <br>";
+    }
+}
+
+//2
+$result = $db->Produit->find(["numero" => 6]);
+echo "<br> 2) <br>";
+foreach ($result as $r) {
+    echo "Produit trouvé :" . $r['numero'] . " | Catégorie : " . $r['categorie'] . " | Libelle : " . $r['libelle'] . " | Description : " . $r['description'];
+    echo " Tarifs : ";
+    foreach ($r['tarifs'] as $tarif) {
+        echo $tarif['taille'] . ": " . $tarif['tarif'] . "€ ";
+    }
+}
+
+//3
+$result = $db->Produit->find(["tarifs.taille" => "normale", "tarifs.tarif" => ['$lte' => 3.0]]);
+echo "<br> 3)";
+foreach ($result as $a) {
+    echo "<br> Produit trouvé :" . $a['numero'] . " | Catégorie : " . $a['categorie'] . " | Libelle : " . $a['libelle'] . " | Description : " . $a['description'];
+    echo " Tarifs : ";
+    foreach ($r['tarifs'] as $tarif) {
+        echo $tarif['taille'] . ": " . $tarif['tarif'] . "€ ";
+    }
+}
